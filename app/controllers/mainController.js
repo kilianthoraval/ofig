b93670b110dd7b3c3f83844fda70033c69e3f8a4
kilianthoraval <kthoraval@gmail.com>
2022-11-16 14:57:31 +0100

    étape 4 dyna et param

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,17 +1,36 @@
 const path = require('path');
+const dataMapper = require("../datamapper");
 
 const mainController = {
+ homePage: async (req, res) => {
 
-  // méthode pour la page d'accueil
-  homePage: (req, res) => {
-    res.render("accueil");
+    const figurines = await dataMapper.getAllFigurines();
+    if (figurines) {
+        res.render("accueil", {
+          figurines
+        });
+    }
+    else {
+    res.status(500).render("500");
+    }
+      
+    
   },
 
-  // méthode pour la page article
-  articlePage: (req, res) => {
-    res.render("article");
-  }
-
+  articlePage: async (req, res) => {
+    const figurineID = req.params.id;
+    const figurine = await dataMapper.getOneFigurine(figurineID);
+ 
+    if (figurine) {
+       
+        res.render("article", {
+            figurine
+        });
+    }
+    else {
+        res.status(500).render("500");
+    }
+}
 };
 
 
--- app/datamapper.js
+++ app/datamapper.js
@@ -2,31 +2,21 @@ const client = require('./database');
 
 const dataMapper = {
    async getAllFigurines(){
-        const sqlQuery = `SELECT * FROM figurine;`;
-        let result; // result est le retour de ma méthode
+        const sqlQuery = "SELECT * FROM figurine;";
+        let result;
 
-        try { // le code a surveillé / ce qui peut planter
-
-            // await va permettre d'attendre la réponse du serveur de BDD avant de continuer
+        try {
             const sqlResult = await client.query(sqlQuery);
             result = sqlResult.rows;
         }
         catch (error) {
-            // comment on a réagi en cas d'erreur
-
-            // 1- Logguer l'erreur
             console.error(error);
         }
-
-        /*
-            si tout se passe bien, result est un tableau de contacts
-            s'il y a un soucis, result est null
-        */
         return result;
     },
 
     async getOneFigurine(id){
-        const sqlQuery = `SELECT * FROM contact WHERE id=${id};`;
+        const sqlQuery = `SELECT * FROM figurine WHERE id=${id};`;
 
         let result;
         try {
@@ -36,8 +26,6 @@ const dataMapper = {
         catch (error) {
             console.error(error);
         }
-
-        // console.log("getContact : ", result.rows);
         return result;
 
     }
